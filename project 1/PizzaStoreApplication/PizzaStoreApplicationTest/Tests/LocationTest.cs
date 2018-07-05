6850857e3708f052d4aed4a31a092cecfbf79d0e
ajzoell <ajzoellner@gmail.com>
2018-07-05 10:32:38 -0400

    Moved models into a folder and began creating tests for methods (deleted old tests as they were not usable).

---
+++ project 1/PizzaStoreApplication/PizzaStoreApplicationTest/Tests/LocationTest.cs
@@ -0,0 +1,97 @@
+﻿using PizzaStoreApplicationLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PizzaStoreApplicationTest
+{
+    public class LocationTest
+    {
+        [Fact]
+        public void ResupplyShouldFillDoughTo1000()
+        {
+            double expected = 1000;
+
+            Location TestLocation = new Location("Test");
+            TestLocation.Dough = 0;
+
+            TestLocation.Resupply();
+            double actual = TestLocation.Dough;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ResupplyShouldFillSauceTo1000()
+        {
+            double expected = 1000;
+
+            Location TestLocation = new Location("Test");
+            TestLocation.Sauce = 0;
+
+            TestLocation.Resupply();
+            double actual = TestLocation.Sauce;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ResupplyShouldFillCheeseTo1000()
+        {
+            double expected = 1000;
+
+            Location TestLocation = new Location("Test");
+            TestLocation.Cheese = 0;
+
+            TestLocation.Resupply();
+            double actual = TestLocation.Cheese;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ResupplyShouldFillPepperoniTo1000()
+        {
+            double expected = 1000;
+
+            Location TestLocation = new Location("Test");
+            TestLocation.Pepperoni = 0;
+
+            TestLocation.Resupply();
+            double actual = TestLocation.Pepperoni;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ResupplyShouldFillPeppersAndOnionsTo1000()
+        {
+            double expected = 1000;
+
+            Location TestLocation = new Location("Test");
+            TestLocation.PeppersAndOnions = 0;
+
+            TestLocation.Resupply();
+            double actual = TestLocation.PeppersAndOnions;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ResupplyShouldFillHamandMeatballTo1000()
+        {
+            double expected = 1000;
+
+            Location TestLocation = new Location("Test");
+            TestLocation.HamAndMeatball = 0;
+
+            TestLocation.Resupply();
+            double actual = TestLocation.HamAndMeatball;
+
+            Assert.Equal(expected, actual);
+        }
+
+
+    }
+}
