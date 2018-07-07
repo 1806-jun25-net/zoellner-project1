acbbefcf2af5c9783b7cc150f8b87a8d10fe3412
ajzoell <ajzoellner@gmail.com>
2018-07-07 12:41:02 -0400

    Created LocationRepo and added method for updating locations (for updating inventory)

---
+++ project 1/PizzaStoreApplication/PizzaStoreApplicationLibrary/Repos and Mapper/LocationRepo.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PizzaStoreApplicationLibrary.Repos_and_Mapper
+{
+    public class LocationRepo
+    {
+        private readonly Project1PizzaApplicationContext _db;
+
+        public LocationRepo(Project1PizzaApplicationContext db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        public IEnumerable<StoreLocation> GetLocations()
+        {
+            List<StoreLocation> locations = _db.StoreLocation.AsNoTracking().ToList();
+            return locations;
+        }
+
+        public void EditLocation(StoreLocation location)
+        {
+            _db.Update(location);
+            _db.SaveChanges();
+        }
+    }
+}
